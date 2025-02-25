3c4fe69bc123f84bf97e7b07cd0fdab2bb25fb93
Salman-Ali05 <sallukhan0805@gmail.com>
2025-02-25 15:16:46 +0100

    next js tree fix

---
+++ app/login/page.js
@@ -0,0 +1,60 @@
+"use client";
+
+import React from 'react';
+import { useRouter } from "next/navigation";
+import Link from "next/link";
+import PageNameWhite from '../components/PageNameWhite';
+import styles from "./login.module.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEye } from '@fortawesome/free-solid-svg-icons';
+
+const LoginPage = () => {
+    const router = useRouter();
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        router.push('/str_home');
+    };
+
+    return (
+        <div className="container">
+            <div className={styles["connection-form"]}>
+                <form className="forms" onSubmit={onSubmit}>
+                    <h2 className={styles["log-title"]}>Connexion</h2>
+
+                    <div className={styles["form-group"]}>
+                        <label>Email</label>
+                        <input type="email" className="inputs" placeholder="Email" name="email" />
+                    </div>
+
+                    <div className={styles["form-group"]}>
+                        <div className={styles["password-label-container"]}>
+                            <label>Mot de passe</label>
+                            <div className={styles["reveal-password"]}>
+                                <span><p>Afficher</p></span>
+                                <span><FontAwesomeIcon icon={faEye} className={styles["icons icons-gray-light"]} /></span>
+                            </div>
+                        </div>
+                        <input type="password" className="inputs" placeholder="Mot de passe" />
+                        <a href="/#" className={styles["forgot-password"]}>Mot de passe oublié</a>
+                    </div>
+
+                    <button type="submit" className="buttons-primary">Se connecter</button>
+                </form>
+
+                <div className={styles["shuffle-form"]}>
+                    <p>Pas encore inscrit ? <Link href="/register">S'inscrire</Link></p>
+                </div>
+            </div>
+
+            <div className={styles["app-name-form"]}>
+
+                <PageNameWhite />
+
+            </div>
+
+        </div>
+    );
+};
+
+export default LoginPage;
--- app/page.js
+++ app/page.js
@@ -1,18 +1,5 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import LoginPage from "./pages/LoginPage";
-import RegisterPage from "./pages/RegisterPage";
-import StructureHome from "./pages/structures/StructureHome";
-// import NotFoundPage from "./pages/NotFoundPage";
+import { redirect } from "next/navigation";
 
-export default function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/str_home" element={<StructureHome />} />
-        {/* <Route path="*" element={<NotFoundPage />} /> */}
-      </Routes>
-    </Router>
-  );
+export default function Home() {
+  redirect("/login");
 }
